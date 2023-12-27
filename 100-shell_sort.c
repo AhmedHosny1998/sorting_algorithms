dfe47c8a21b17fa08c5dca092933c568d488acaa
AhmedHosny1998 <hosny4620Qgmail.com>
2023-12-27 08:46:44 -0800

    sd

---
+++ 100-shell_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+
+/**
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t getMaxgap(size_t size){
+
+    size_t n = 1;
+    while (n < size)
+    {
+        n = n * 3 + 1;
+        return ((n - 1 ) / 3);
+    }
+}
+
+void shell_sort(int *array, size_t size)
+{
+    size_t gap , i , j;
+    int tmp;
+
+    if(!array || !size)
+    {
+        return;
+    }
+    for (gap = getMaxgap(size); gap; gap = (gap - 1)/3)
+    {
+        for (i = gap ; i < size; i++){
+            tmp = array[i];
+            for (j = i ; j > gap -1 && array[j - gap] > tmp ; j -= gap)
+            {
+                array[j] = array[j - gap];
+            }
+            array[j] = tmp;
+        }
+        print_array(array , size);
+    }
+}
