40021da1b78a0626c28661297e14d7199f6f58a1
AhmedHosny1998 <hosny4620Qgmail.com>
2023-12-30 20:14:06 -0800

    merge

---
+++ 103-merge_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+
+
+void merge_compare(int *array, size_t start, size_t stop, int *new)
+{
+    size_t i = start, j, k, midel;
+
+    j = midel = (start + stop) / 2;
+    printf("Merging...\n");
+	printf("[left]: ");
+    print_array(array + start, midel-start);
+    printf("[right]: ");
+    print_array(array + midel, stop - midel);
+    for (k = start; k < stop; k++)
+    {
+        if (i < midel && (j >= stop || array[i] <= array[j]))
+        {
+            new[k] = array[i++];
+        }
+
+        else
+        {
+            new[k] = array[j++];
+        }
+
+        printf("[Done]: ");
+        print_array(new + start, stop - start);
+    }
+    
+
+}
+
+void merge_sort_top_down(int *array, size_t start, size_t stop, int *new)
+{
+    size_t midel;
+    midel = (start + stop) / 2;
+    if (stop - start < 2)
+    {
+        return;
+    }
+
+    merge_sort_top_down(new, start, midel, array);
+    merge_sort_top_down(new, midel, stop, array);
+
+    
+
+
+}
+
+void merge_sort(int *array, size_t size)
+{
+
+    int *new;
+    size_t i;
+    
+    if(!array || size < 2)
+    {
+        return;
+    }
+
+    new = malloc(sizeof(int) * size);
+    if(!new)
+    {
+        return;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        new[i] = array[i];
+    }
+
+    merge_sort_top_down(array, 0, size, new);
+    free(new);
+
+}
+
