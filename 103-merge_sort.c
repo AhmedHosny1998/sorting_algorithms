8a0845cf873b0221d2a4bdfa175ffeac9cfd14dd
AhmedHosny1998 <hosny4620Qgmail.com>
2023-12-30 20:19:36 -0800

    merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -9,34 +9,28 @@
  *
  * Return: void.
  */
-
 void merge_compare(int *array, size_t start, size_t stop, int *new)
 {
-    size_t i = start, j, k, midel;
+	size_t i = start, j, k, mid;
 
-    j = midel = (start + stop) / 2;
-    printf("Merging...\n");
+	j = mid = (start + stop) / 2;
+	printf("Merging...\n");
 	printf("[left]: ");
-    print_array(array + start, midel-start);
-    printf("[right]: ");
-    print_array(array + midel, stop - midel);
-    for (k = start; k < stop; k++)
-    {
-        if (i < midel && (j >= stop || array[i] <= array[j]))
-        {
-            new[k] = array[i++];
-        }
+	print_array(array + start, mid - start);
+	printf("[right]: ");
+	print_array(array + mid, stop - mid);
+	for (k = start; k < stop; k++)
+		if (i < mid && (j >= stop || array[i] <= array[j]))
+		{
 
-        else
-        {
-            new[k] = array[j++];
-        }
-
-        printf("[Done]: ");
-        print_array(new + start, stop - start);
-    }
-    
-
+			new[k] = array[i++];
+		}
+		else
+		{
+			new[k] = array[j++];
+		}
+	printf("[Done]: ");
+	print_array(new + start, stop - start);
 }
 
 /**
@@ -48,23 +42,20 @@ void merge_compare(int *array, size_t start, size_t stop, int *new)
  *
  * Return: void.
  */
-
 void merge_sort_top_down(int *array, size_t start, size_t stop, int *new)
 {
-    size_t midel;
-    midel = (start + stop) / 2;
-    if (stop - start < 2)
-    {
-        return;
-    }
+	size_t mid;
 
-    merge_sort_top_down(new, start, midel, array);
-    merge_sort_top_down(new, midel, stop, array);
-
-    
-
-
+	mid = (start + stop) / 2;
+	if (stop - start < 2)
+	{
+		return;
+	}
+	merge_sort_top_down(new, start, mid, array);
+	merge_sort_top_down(new, mid, stop, array);
+	merge_compare(new, start, stop, array);
 }
+
 
 /**
  * merge_sort - sorts by merge sort algorithm
@@ -73,31 +64,20 @@ void merge_sort_top_down(int *array, size_t start, size_t stop, int *new)
  *
  * Return: void.
  */
-
 void merge_sort(int *array, size_t size)
 {
+	int *new;
+	size_t i;
 
-    int *new;
-    size_t i;
-    
-    if(!array || size < 2)
-    {
-        return;
-    }
 
-    new = malloc(sizeof(int) * size);
-    if(!new)
-    {
-        return;
-    }
+	if (!array || size < 2)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
-        new[i] = array[i];
-    }
-
-    merge_sort_top_down(array, 0, size, new);
-    free(new);
-
+	new = malloc(sizeof(int) * size);
+	if (!new)
+		return;
+	for (i = 0; i < size; i++)
+		new[i] = array[i];
+	merge_sort_top_down(array, 0, size, new);
+	free(new);
 }
-
